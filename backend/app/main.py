816471e7282e835a4ab291d971538530eb9d57fd
夏书培 <13935078+xsp10086@user.noreply.gitee.com>
2026-09-09 13:32:23 +0800

    Revert "chore: bump version to 0.1.1"
    This reverts commit 121824f9f8cb9dbd5da8a6b09c6c14ad58947d8d.

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(title=settings.app_name, version="0.1.1", lifespan=lifespan)
+app = FastAPI(title=settings.app_name, version="0.1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
